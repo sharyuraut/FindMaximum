cbe4cb4a62e34021c2fa9137f25907eae78edce1
sharyu raut <sharyukraut210298@gmail.com>
2020-10-21 01:07:55 +0530

    [Sharyu] Add. uc2 find max float value and added test cases

--- FindMaximum/Program.cs
+++ FindMaximum/Program.cs
@@ -28,12 +28,37 @@ namespace FindMaximum
             }
             return a;
         }
+
+        public static double GetMaxFloat(double a, double b, double c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 ||
+                a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 ||
+                a.CompareTo(b) > 0 && a.CompareTo(c) >= 0)
+            {
+                return a;
+            }
+
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 ||
+                b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 ||
+                b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
+            {
+                return b;
+            }
+
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0 ||
+                c.CompareTo(a) >= 0 && c.CompareTo(b) > 0 ||
+                c.CompareTo(a) > 0 && c.CompareTo(b) >= 0)
+            {
+                return c;
+            }
+            return a;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Find Maximum program!");
 
-            Console.WriteLine(GetMaxInt(4, 2, 8));
-            //Console.WriteLine(GetMaxFloat(11.4, 334.6, 234.8));
+            Console.WriteLine("Maximum Integer: " + GetMaxInt(4, 2, 8));
+            Console.WriteLine("Maximum Float Value: " + GetMaxFloat(11.4, 334.6, 234.8));
             //Console.WriteLine(GetMaxString("12", "25", "23"));
         }
     }
--- TestMaximumWithGenerics/UnitTest1.cs
+++ TestMaximumWithGenerics/UnitTest1.cs
@@ -38,5 +38,29 @@ namespace TestMaximumWithGenerics
 
             Assert.AreEqual(45, max);
         }
+
+        //TC 2.1
+        [TestMethod]
+        public void GivenDouble_MaxFirstNumber_ShouldReturnFirstNumber()
+        {
+            double max = Program.GetMaxFloat(30.5, 12.9, 18.6);
+            Assert.AreEqual(30.5, max);
+        }
+
+        //TC 2.2
+        [TestMethod]
+        public void GivenDouble_MaxSecondNumber_ShouldReturnSecondNumber()
+        {
+            double max = Program.GetMaxFloat(16.5, 50.8, 24.9);
+            Assert.AreEqual(50.8, max);
+        }
+
+        //TC 2.3
+        [TestMethod]
+        public void GivenDouble_MaxThirdNumber_ShouldReturnThirdNumber()
+        {
+            double max = Program.GetMaxFloat(15.9, 20.2, 45.5);
+            Assert.AreEqual(45.5, max);
+        }
     }
 }
